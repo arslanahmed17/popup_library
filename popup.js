53993f2bd5f6c2dbbb6b028c8b0cb1234743c0b5
arslanahmed17 <40499781+arslanahmed17@users.noreply.github.com>
2018-06-25 12:49:43 +0500

    popup_task

---
+++ popup.js
@@ -0,0 +1,13 @@
+function popup(){
+    
+	var data = document.getElementById("text");
+    
+	if ( data.style.display === "none" )
+	{ 
+        data.style.display = "block";
+    } 
+    else
+	{
+        data.style.display = "none";
+    }
+}
